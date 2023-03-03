a40c8e5cdf59024ea22a3aa2d0083ad93e2a15b8
abhay nepal <abhay.nepal.9@gmail.com>
2023-03-03 19:08:55 +0545

    pure mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CalisthenicsRoutineTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
